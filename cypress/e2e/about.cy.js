c8fd9a886d2a0a835dde86a3db7646262190fd74
Brandon Ainsworth <brandonaainsworth@gmail.com>
2022-08-07 18:19:44 -0600

    Add tests for about page

--- cypress/e2e/about.cy.js
+++ cypress/e2e/about.cy.js
@@ -1,5 +1,13 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('About', () => {
+  
+  it('Should render all relevant components', () => {
+    cy.visit('http://localhost:3000/about')
+    cy.get('[data-cy="emoji"]').should('have.length', 2)
+    cy.get('[data-cy="header-h1"]')
+    cy.get('[data-cy="nav-elem"]').should('have.length', 4)
+    cy.get('.about-header')
+    cy.get('.about-subhead')
+    cy.get('.imageme-header')
+    cy.get('.about')
   })
 })
